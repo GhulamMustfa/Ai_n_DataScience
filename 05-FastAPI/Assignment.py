9337a16454e48ab4061ff1a0489f291b31aa44f9
Ghulam Mustfa <mustfa18gm@gmail.com>
2025-01-25 13:16:40 +0500

    Assignment FastAPI

--- 05-FastAPI/Assignment.py
+++ 05-FastAPI/Assignment.py
@@ -3,22 +3,44 @@
 # poetry run uvicorn 05-FastAPI.Assignment:app
 
 from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel # type: ignore
+from typing import Optional
+import re
+
 
 app = FastAPI()
 
+
+class Student(BaseModel):
+    name: str
+    email: str
+    age: int
+    address: list[str]
+
+@app.post("/students/register")
+def register_student(student: Student):
+    return {"student": student}
+    
+
+
+@app.post("/students/{student_id}/email")
+def update_email(student_id: int, email: str):
+    return {"student_id": student_id,
+            "email": email}
+
+
+
 @app.get("/")
-def get_hello_world():
+def get_student_info():
     return {"Get": "Student Information"}
 
-@app.get("/students/{student_id}")
-def get_studentID(student_id: int):
-    if student_id < 1000 or student_id > 9999:
-        return("ID should be between 1000 and 9999")
-        # raise ValueError("ID should be between 1000 and 9999")
-    return {"student_id": student_id}
 
-@app.get("/grades")
-def grades_semester(q: bool, semester: str):
-    return {"query": q, 
+@app.get("/students/{student_id}")
+def get_studentID(student_id: int, include_grades: bool, semester: str = r"^(Fall|Spring|Summer)\d{4}$"):
+    if student_id < 1000 or student_id > 9999:
+        return {"Error": "ID should be between 1000 and 9999"}
+        # raise ValueError("ID should be between 1000 and 9999")
+    return {"student_id": student_id,
+            "include_grades": include_grades,
             "semester": semester}
 
