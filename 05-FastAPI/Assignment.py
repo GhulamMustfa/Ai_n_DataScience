75b6f7268a3af516a8125439a7223af79598302b
Ghulam Mustfa <mustfa18gm@gmail.com>
2025-01-26 15:41:51 +0500

    FastAPI Assignment

--- 05-FastAPI/Assignment.py
+++ 05-FastAPI/Assignment.py
@@ -2,45 +2,41 @@
 
 # poetry run uvicorn 05-FastAPI.Assignment:app
 
-from fastapi import FastAPI # type: ignore
-from pydantic import BaseModel # type: ignore
-from typing import Optional
-import re
+from fastapi import FastAPI, Path, Query # type: ignore
+from pydantic import BaseModel, EmailStr # type: ignore
 
 
 app = FastAPI()
-
-
-class Student(BaseModel):
-    name: str
-    email: str
-    age: int
-    address: list[str]
-
-@app.post("/students/register")
-def register_student(student: Student):
-    return {"student": student}
-    
-
-
-@app.post("/students/{student_id}/email")
-def update_email(student_id: int, email: str):
-    return {"student_id": student_id,
-            "email": email}
-
-
 
 @app.get("/")
 def get_student_info():
     return {"Get": "Student Information"}
 
-
 @app.get("/students/{student_id}")
-def get_studentID(student_id: int, include_grades: bool, semester: str = r"^(Fall|Spring|Summer)\d{4}$"):
-    if student_id < 1000 or student_id > 9999:
-        return {"Error": "ID should be between 1000 and 9999"}
-        # raise ValueError("ID should be between 1000 and 9999")
+def get_student_id(student_id: int = Path(ge=1000, le=9999),
+                  include_grades: bool = Query,
+                  semester: str = Query(None, regex="^(Fall|Spring|Summer)\d{4}$")):
     return {"student_id": student_id,
             "include_grades": include_grades,
             "semester": semester}
 
+
+class Student(BaseModel):
+    name: str
+    email: EmailStr
+    age: int = Path(ge=18, le=30)
+    course: list[str]
+
+@app.post("/students/register")
+def register_student(student: Student):
+    return{student: student}
+
+@app.put("/students/{student_id}/email")
+def update_email(email: str, student_id: int = Path(ge=1000, le=9999)):
+    return {"student_id": student_id,
+            "email": email}
+
+
+
+
+# , name: str, email: EmailStr, age: int, course: list[str]
