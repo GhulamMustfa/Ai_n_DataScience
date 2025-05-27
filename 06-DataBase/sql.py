1fc09ba1818c74a8e3022e9dbe7740953659a110
Ghulam Mustfa <mustfafsd41@gmail.com>
2025-05-27 13:11:55 +0500

    practice DB:

--- 06-DataBase/sql.py
+++ 06-DataBase/sql.py
@@ -4,20 +4,67 @@ from fastapi import FastAPI  # type: ignore
 from pymongo import MongoClient  # type: ignore
 from dotenv import load_dotenv # type: ignore
 import os
+from pydantic import BaseModel # type: ignore
+import datetime
+# from bson.objectid import ObjectId # type: ignore
 
 app = FastAPI()
 
+
 load_dotenv()
-client = MongoClient(os.getenv("DB_URI"))
+# client = MongoClient(os.getenv("DB_URI"))
+# print(os.getenv("DB_URI"))
+def check_db_client():
+    try:
+        client = MongoClient(os.getenv("DB_URI"))
+        print("Connected to DataBase")
+        return client
+    except Exception as e:
+        print(f"Error connecting to DataBase{e}")
+        return None
 
-print(os.getenv("DB_URI"))
 
+client = check_db_client()
 db = client["Learning"]
 collection = db["FastAPI"]
 
+class ToDo(BaseModel):
+    title: str
+    description: str
+    status: str
+
 
 @app.get("/")
-def read_root():
+def check_server():
     return {"Status": "Server is running"}
 
 
+@app.get("/todos")
+def read_todos():
+    try:
+        todos = db.todos.find()
+        # listtodos = []
+        # for todo in todos():
+        #     listtodos.append({
+        #         "id": str(todo["_id"]),
+        #         "title": todo["title"],
+        #         "description": todo["description"],
+        #         # "status": todo["status"],
+        #         "created_at": todo["created at"],
+        #         "completed": todo["completed"]
+        #     })
+        return db.todos
+        # return {
+        #     "data": listtodos,
+        #     "error": None,
+        #     "message": "Todos read successfully",
+        #     "status": "success"
+        #     }
+    except Exception as e:
+        print(f"Error reading todos: {e}")
+        return {
+            "data": [],
+            "error": "Error reading todos",
+            "message": str(e),
+            "status": "failed"
+            }
